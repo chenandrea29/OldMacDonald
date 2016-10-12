e73ca65dcb0ffec14a4236a2153f221553427a61
chenandrea29 <chenandrea29@gmail.com>
2016-10-12 14:35:31 -0700

    Added second sound to chick

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,12 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+	private String myName;
+	Cow(String type, String name, String sound) {
+		myType = type;
+		myName = name;
+		mySound = sound;
+	}
+	public String getName() {
+		return myName;
+	}
 }
